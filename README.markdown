386e96c669d2afe594f380a5d13ba37e94b533ab
AlexZhang <blackanger.z@gmail.com>
2011-06-27 14:51:05 +0800

    modify README

--- README.markdown
+++ README.markdown
@@ -1,6 +1,8 @@
 jQuery captcha system with jQuery & jQuery UI for Rails 3
 =========================================================
 
+__modify from:[QapTcha](http://www.myjqueryplugins.com/QapTcha "Title")__
+
 Support Env:
 ------------
 Rails3.0.8+/Ruby1.8.7+
@@ -9,3 +11,4 @@ Usage:
 ------
 
 1. rails g rails_qaptcha:install
+2. 
--- lib/generators/rails_qaptcha/install_generator.rb
+++ lib/generators/rails_qaptcha/install_generator.rb
@@ -12,9 +12,12 @@ module RailsQaptcha
       source_root File.expand_path('../media', __FILE__)
       desc "Copies media files to main project"
       def copy_mongoid_yml_files
-        copy_file "../media/images/*",      "public/images/qaptcha/*"
-        copy_file "../media/javascripts/*", "public/javascripts/qaptcha/*"
-        copy_file "../media/stylesheets/*", "public/stylessheets/qaptcha/*"
+        copy_file "../media/images/bg_QapTcha.png",          "public/images/qaptcha/bg_QapTcha.png"
+        copy_file "../media/images/sprites.png",             "public/images/qaptcha/sprites.png"
+        copy_file "../media/javascripts/jquery-ui.js",       "public/javascripts/qaptcha/jquery-ui.js"
+        copy_file "../media/javascripts/jquery.ui.touch.js", "public/javascripts/qaptcha/jquery.ui.touch.js"
+        copy_file "../media/javascripts/QapTcha.jquery.js",  "public/javascripts/qaptcha/QapTcha.jquery.js"
+        copy_file "../media/stylesheets/QapTcha.jquery.css", "public/stylessheets/qaptcha/QapTcha.jquery.css"
       end
     end
 
