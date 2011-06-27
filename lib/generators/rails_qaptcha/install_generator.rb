3ed6a8a20aa6bb349a51a6a78c1a2ce5eeb30021
AlexZhang <blackanger.z@gmail.com>
2011-06-27 15:51:04 +0800

    modify images

--- lib/generators/rails_qaptcha/install_generator.rb
+++ lib/generators/rails_qaptcha/install_generator.rb
@@ -12,8 +12,8 @@ module RailsQaptcha
       source_root File.expand_path('../media', __FILE__)
       desc "Copies media files to main project"
       def copy_mongoid_yml_files
-        copy_file "../media/images/bg_QapTcha.png",          "public/images/qaptcha/bg_QapTcha.png"
-        copy_file "../media/images/sprites.png",             "public/images/qaptcha/sprites.png"
+        copy_file "../media/images/bg_QapTcha.png",          "public/stylesheets/image/qaptcha/bg_QapTcha.png"
+        copy_file "../media/images/sprites.png",             "public/stylesheets/image/qaptcha/sprites.png"
         copy_file "../media/javascripts/jquery-ui.js",       "public/javascripts/qaptcha/jquery-ui.js"
         copy_file "../media/javascripts/jquery.ui.touch.js", "public/javascripts/qaptcha/jquery.ui.touch.js"
         copy_file "../media/javascripts/QapTcha.jquery.js",  "public/javascripts/qaptcha/QapTcha.jquery.js"
