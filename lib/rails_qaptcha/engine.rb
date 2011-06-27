407c6dc64e034ecf354bc965ba30730363334062
AlexZhang <blackanger.z@gmail.com>
2011-06-27 15:10:20 +0800

    modify engine file

--- lib/rails_qaptcha/engine.rb
+++ lib/rails_qaptcha/engine.rb
@@ -1,8 +1,14 @@
 require 'rails'
 
-
 module RailsQaptcha
   class Engine < Rails::Engine
-    # ...
+    
+    # =====================================
+    # = = javascript_include_tag :qaptcha =
+    # =====================================
+    config.before_initialize do
+      config.action_view.javascript_expansions[:qaptcha] = %w(jquery-ui.js jquery.ui.touch.js QapTcha.jquery.js)
+    end
+    
   end
 end
